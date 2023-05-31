c392ec6443179d66d208f1e05ab77e125ead373a
fournoas <admin@fournoas.com>
2023-05-31 10:06:54 +0800

    winsvc patch

--- nuitka/build/SconsCompilerSettings.py
+++ nuitka/build/SconsCompilerSettings.py
@@ -735,7 +735,7 @@ def setupCCompiler(env, lto_mode, pgo_mode, job_count):
     # we didn't manage to have a "shell32.lib" that is not considered corrupt, so we
     # have to do this.
     if env.msvc_mode and env.target_arch != "arm64":
-        env.Append(LIBS=["Shell32"])
+        env.Append(LIBS=["Shell32", "Advapi32"])
 
     # Since Fedora 36, the system Python will not link otherwise.
     if isFedoraBasedLinux():
