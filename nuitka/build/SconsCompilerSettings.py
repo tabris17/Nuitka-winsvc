b9a9f9b0f85a2ea9c4cf41d6f33d53f529109581
fournoas <admin@fournoas.com>
2025-06-10 18:30:43 +0800

    winsvc patch

--- nuitka/build/SconsCompilerSettings.py
+++ nuitka/build/SconsCompilerSettings.py
@@ -874,7 +874,7 @@ def setupCCompiler(env, lto_mode, pgo_mode, job_count, onefile_compile):
 
     if env.msvc_mode:
         # For shell API usage to lookup app folders we need this.
-        env.Append(LIBS=["Shell32"])
+        env.Append(LIBS=["Shell32", "Advapi32"])
 
         # Make sure to find MSVC "link" in the right spot and to not use the
         # "link" of e.g. Git.
